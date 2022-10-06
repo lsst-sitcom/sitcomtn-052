2408436d10cff791d4f2524c112a3a6e6e17ae11
William O'Mullane <womullan@lsst.org>
2022-10-06 16:30:15 -0700

    python 3.10
     python 3.10

--- conf.py
+++ conf.py
@@ -3,9 +3,11 @@
 # Sphinx configuration file
 # see metadata.yaml in this repo to update document-specific metadata
 
+from documenteer.conf.technote import * # noqa: F401, F403
+
 import datetime
 import os
-from documenteer.sphinxconfig.technoteconf import configure_technote
+#from documenteer.sphinxconfig.technoteconf import configure_technote
 
 # Ingest settings from metadata.yaml and use documenteer's configure_technote()
 # to build a Sphinx configuration that is injected into this script's global
@@ -23,9 +25,3 @@ g = globals()
 g.update(confs)
 
 
-# Add intersphinx inventories as needed
-# http://www.sphinx-doc.org/en/stable/ext/intersphinx.html
-# Example:
-#
-#     intersphinx_mapping['python'] = ('https://docs.python.org/3', None)
-html_logo = '_static/rubin_logo_white.png'
