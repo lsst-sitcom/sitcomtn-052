593ff1c7d59f5887cf780683618c27f0dc57336f
William O'Mullane <womullan@lsst.org>
2022-10-06 16:35:31 -0700

    remove estra conf
     logo
    
     logo

--- conf.py
+++ conf.py
@@ -5,23 +5,5 @@
 
 from documenteer.conf.technote import * # noqa: F401, F403
 
-import datetime
-import os
-#from documenteer.sphinxconfig.technoteconf import configure_technote
-
-# Ingest settings from metadata.yaml and use documenteer's configure_technote()
-# to build a Sphinx configuration that is injected into this script's global
-# namespace.
-metadata_path = os.path.join(os.path.dirname(__file__), 'metadata.yaml')
-with open(metadata_path, 'r') as f:
-    confs = configure_technote(f)
-
-# Set revision date to the current date.
-# The “true” revision is really set by the version of the data used in the
-# milestones repository, but that's awkward to work with.
-confs['last_revised'] = datetime.datetime.now().strftime("%Y-%m-%d")
-
-g = globals()
-g.update(confs)
-
+html_logo = '_static/rubin_logo.png'
 
